a39a350d612da0a2fdda798e5c6583c34f4babd6
Bart <frayhalos@gmail.com>
2022-07-29 22:45:20 -0500

    Modulo de Usuarios (Listar,Paginacion y Busqueda Echa)

---
+++ SistemaFront/src/components/Rol.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-layout aligm-start>
+    <v-flex>
+      <v-data-table
+        :headers="Tabla_Encabezados"
+        :items="roles"
+        sort-by="calories"
+        class="elevation-1"
+        :search="buscar"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>Gestión de Roles</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+              <v-text-field class="text-xs-center" v-model="buscar" append-icon="search" label="Búsqueda" single-line hide-details></v-text-field>
+            <v-spacer></v-spacer>
+          
+           
+            
+          </v-toolbar>
+        </template>
+
+        <template v-slot:item="props">
+          <tr>
+            <td>{{ props.item.nombre }}</td>
+            <td>{{ props.item.descripcion }}</td>
+          </tr>
+        </template>
+
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="listar">Actualizar </v-btn>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    roles: [],
+    Tabla_Encabezados: [
+      { text: "Nombre", value: "nombre" },
+      { text: "Descripción", value: "descripcion", sortable: false },
+      { text: "Estado", value: "condicion", sortable: false },
+    ],
+    buscar: '',
+    
+  }),
+
+  computed: {
+    
+  },
+
+  created() {
+    this.listar();
+  },
+
+  methods: {
+    listar() {
+      let datos = this;
+      axios
+        .get("api/Roles/Listar")
+        .then(function (response) {
+          //console.log(response);
+            datos.roles = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },   
+  },
+};
+</script>
---
+++ SistemaFront/src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Categoria_Component from '../components/Categoria.vue'
+import Articulos_Component from '../components/Articulos.vue'
+import Rol_Component from '../components/Rol.vue'
+import Usuarios_Component from '../components/Usuario.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/categorias',
+    name: 'categorias',
+    component: Categoria_Component
+  },
+  {
+    path: '/articulos',
+    name: 'articulos',
+    component: Articulos_Component
+  },
+  {
+    path: '/roles',
+    name: 'roles',
+    component: Rol_Component
+  },
+  {
+    path: '/usuarios',
+    name: 'usuarios',
+    component: Usuarios_Component
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
